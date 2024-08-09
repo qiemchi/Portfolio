e81185624d43c26da94e2ad83028b2734ab19cd4
qiemchi <qiewifruit@gmail.com>
2024-08-09 22:26:30 +0700

    added subheading

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/TextGenerateEffect'
 
 const Hero = () => {
   return (
@@ -13,18 +14,27 @@ const Hero = () => {
             fill="blue" />
         </div>
 
-        <div className="h-screen w-full dark:bg-black bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+        <div className="h-screen w-full dark:bg-black bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center
          dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
         </div>
 
-        <div className = "flex justify-center">
-            <div className = 'max-w-[89vw]'>
+        <div className = "flex justify-center relative my-20 z-10">
+            <div className = 'max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex-col items-center justify-center'>
                 <h2 className = 
                 "uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
                     Dynamic Web Magic with Next.js
                 </h2>
+
+                <TextGenerateEffect
+                    className = "text-center text-[40px] md:text-5xl lg:text-6xl"
+                    words = "Transforming Concept into Seamless Experiences"
+                />
+
+                <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+                    Hi, I'm Qie, a Next.js Developer based in Indonesia.
+                </p>
             </div>
         </div>
     </div>
