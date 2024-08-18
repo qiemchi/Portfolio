97e30d30c50c03a3dee14f7abebb822ec3e878df
qiemchi <qiewifruit@gmail.com>
2024-08-18 14:07:00 +0700

    added approach section

---
+++ components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="w-full pt-20 pb-10" id="contact">
+        <div>
+            <img 
+                src="/footer-grid.svg"
+                alt="grid"
+                className="w-full h-full opacity-50"
+            />
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
