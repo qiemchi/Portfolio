4aed1ca2e51c879f5f3d0f0bce1fb07cf2b7cbeb
qiemchi <qiewifruit@gmail.com>
2024-08-18 23:59:34 +0700

    minor fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -31,7 +31,7 @@ const Footer = () => {
         </div>
 
         <div className="flex mt-16 md:flex-row flex-col justify-between items-center gap-y-8">
-            <p className="md:text-base text-sm md:font-normal font-light">Copright © 2024 Qie</p>
+            <p className="md:text-base text-sm md:font-normal font-light">Copyright © 2024 Qie</p>
 
             <div className="flex items-center md:gap-3 gap-6">
                 {socialMedia.map((profile) => (
